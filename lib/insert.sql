793d7bcdc560a8aa219043a0f7e86fce8f3d2db9
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-10-15 18:30:25 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,23 +1,23 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
+INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ('Mr. Chocolate', 3, 'M', "Brown", "Nice", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
+INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ('Rowdy', 3, 'M', "Brown", "Nice", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
+INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ('Tabitha', 3, 'M', "Brown", "Nice", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
+INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ('Sergeant Brown', 3, 'M', "Brown", "Nice", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Melissa', 3, 'M', "Brown", "Nice", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ('Mr. Chocolate', 3, 'M', "Brown", "Nice", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
+INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ('Mr. Chocolate', 3, 'M', "Brown", "Nice", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
-VALUES ('Mr. Chocolate', 3, 'M', "Brown", "Nice", 1);
-
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
+INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ('Mr. Chocolate', 3, 'M', "Brown", "Nice", 1);
