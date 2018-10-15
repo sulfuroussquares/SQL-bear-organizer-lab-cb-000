0e47a7358a07d6b07bfc184080a85c2ec9e37243
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-10-15 18:35:02 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,6 +1,5 @@
 def selects_all_female_bears_return_name_and_age
-  "SELECT name, age from bears
-  WHERE gender = F"
+  "SELECT name, age from bears WHERE gender = F"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
