e73361485370215e1ac2c17ea109e0cb29a6c584
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-10-15 18:41:46 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT COUNT from bears GROUP BY color"
+  puts "SELECT COUNT from bears GROUP BY color WHERE color"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
