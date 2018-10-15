61d371a04a6b1703fd5edb9ec1072e5ca9788c06
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-10-15 18:48:11 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,7 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT color, COUNT(id) from bears GROUP BY color ORDER BY color desc LIMIT 1"
+  "SELECT COUNT(id) from bears WHERE temperament = 'goofy'"
 end
 
 def selects_bear_that_killed_Tim
